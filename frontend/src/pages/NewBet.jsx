19d0167f8c58eddc4424f7709372b75e242e6925
Dan <dan.laratta.jr@gmail.com>
2022-09-20 13:41:16 -0400

    new bet page/form

--- frontend/src/pages/NewBet.jsx
+++ frontend/src/pages/NewBet.jsx
@@ -34,10 +34,73 @@ const BodyWrapper = styled.div`
     padding: 8rem 0rem;
 `
 
-const BodyTitle = styled.span`
+const FormTitle = styled.span`
     font-size: 4rem;
     font-weight: bold;
     padding-bottom: 4rem;
+`
+
+const FormContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`
+
+const BetForm = styled.form`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 1rem;
+    -webkit-box-shadow: 0px 0px 15px 3px #959595; 
+    box-shadow: 0px 0px 15px 3px #959595;
+    background-color: #dddddd;
+    padding: 5rem 0rem;
+`
+
+const BetInput = styled.input`
+    width: 50%;
+    margin: 1.5rem 0rem;
+    padding: 1.5rem 2rem;
+    border-radius: 0.5rem;
+    -moz-appearance: textfield;
+    font-size: 1.6rem;
+
+    &::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    &::-webkit-inner-spin-button {
+         -webkit-appearance: none;
+        margin: 0;
+    }
+
+`
+
+const BetSelect = styled.select`
+    width: 53.5%;
+    margin: 1.5rem 0rem;
+    padding: 1.5rem 2rem;
+    border-radius: 0.5rem;
+    font-size: 1.6rem;
+    background-color: #fff;
+`
+
+const Btn = styled.button`
+    margin: 1.5rem 0rem;
+    padding: 1.5rem 5rem;
+    border-radius: 1rem;
+    background-color: #000;
+    outline: none;
+    color: #fff;
+    font-size: 1.6rem;
+    font-weight: 500;
+
+    &:hover{
+        color: gold;
+        cursor: pointer;
+    }
 `
 
 const NewBet = () => {
@@ -52,7 +115,34 @@ const NewBet = () => {
 
                 <BodyContainer>
                     <BodyWrapper>
-                        <BodyTitle> New Bet </BodyTitle>
+                        <FormContainer>
+                            <BetForm>
+                                <FormTitle> New Bet </FormTitle>
+
+                                <BetInput 
+                                    type= 'text'
+                                    placeholder= 'Bet Description'
+                                />
+
+                                <BetInput 
+                                    type= 'number'
+                                    placeholder= 'Bet Wager'
+                                />
+
+                                <BetSelect>
+                                    <option value=''> Bet Outcome </option>
+                                    <option value='Won'> Won </option>
+                                    <option value='Lost'> Lost </option>
+                                </BetSelect>
+
+                                <BetInput 
+                                    type= 'text'
+                                    placeholder= 'Bet Description'
+                                />
+
+                                <Btn type='submit'> Submit </Btn>
+                            </BetForm>
+                        </FormContainer>
 
                     </BodyWrapper>
                 </BodyContainer>
