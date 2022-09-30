82fcb480e84c3f9a053493013ce16eb5e4b225c4
Dan <dan.laratta.jr@gmail.com>
2022-09-30 12:26:40 -0400

    hashrouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import BetStats from './pages/BetStats'
